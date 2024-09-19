676ab801afc115236d00d16c526d068ed806ef95
ftwammes <wammesfernanda@gmail.com>
2024-09-18 21:25:14 -0300

    Ajustes no layout da tela

--- client/classroom/src/app/page.tsx
+++ client/classroom/src/app/page.tsx
@@ -3,16 +3,25 @@
 import React from "react";
 import styles from "./page.module.css";
 import ClassCard from "../components/ClassCard";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
+    
     <div className={styles.page}>
+      <Navbar></Navbar>
       <main className={styles.main}>
         <ClassCard
-          name="Mundo Digital"
-          creatorName="Fernanda"
-          creatorPhoto="" // Atualize o caminho da foto conforme necessário
+          name="Materia 1"
+          creatorName="Professor 1"
+          creatorPhoto="https://down-br.img.susercontent.com/file/br-11134207-7r98o-lykwcwv6r86ddf" 
           id="1"
+        />
+        <ClassCard
+          name="Materia 2"
+          creatorName="Professor 2"
+          creatorPhoto="https://down-br.img.susercontent.com/file/br-11134207-7r98o-lykwcwv6r86ddf" 
+          id="2"
         />
       </main>
     </div>
